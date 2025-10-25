18d26b746f36eea62dc56055e10b5fb9f97136ed
wisdom08 <wisdom08@kakao.com>
2025-10-26 05:04:08 +0900

    feat: 친구 시스템 구현
    - LocalStorageService에 친구 목록 저장/불러오기 기능 추가
    - saveFriends() 메서드 구현
    - getFriends() 메서드 구현
    - SharedPreferences를 통한 친구 데이터 영속화

--- lib/services/local_storage_service.dart
+++ lib/services/local_storage_service.dart
@@ -82,6 +82,16 @@ class LocalStorageService {
     }
   }
 
+  // 친구 목록 저장
+  static Future<void> saveFriends(List<String> friends) async {
+    await prefs.setStringList('friends', friends);
+  }
+
+  // 친구 목록 불러오기
+  static List<String> getFriends() {
+    return prefs.getStringList('friends') ?? [];
+  }
+
   // 모든 데이터 초기화
   static Future<void> clearAll() async {
     await prefs.clear();
